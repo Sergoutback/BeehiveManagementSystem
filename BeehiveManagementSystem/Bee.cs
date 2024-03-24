4e75792c6c6b55e8a61b243a65db79ca07ebf900
Sergoutback <webart2013@gmail.com>
2024-03-24 20:08:29 +0300

    First commit

---
+++ BeehiveManagementSystem/Bee.cs
@@ -0,0 +1,24 @@
+﻿namespace BeehiveManagementSystem;
+
+public class Bee
+{
+    public virtual float CostPerShift { get; }
+    public string Job { get; private set; }
+    public Bee(string job)
+    {
+        Job = job;
+    }
+
+
+    public void WorkTheNextShift()
+    {
+        if (HoneyVault.ConsumeHoney(CostPerShift))
+        {
+            DoJob();
+        }
+
+    }
+
+    protected virtual void DoJob() { /* the subclass overrides this */ }
+
+}
---
+++ BeehiveManagementSystem/HoneyManufacture.cs
@@ -0,0 +1,13 @@
+﻿namespace BeehiveManagementSystem;
+
+public class HoneyManufacture : Bee
+{
+    public const float NECTAR_PROCESSED_PER_SHIFT = 33.15f;
+    public override float CostPerShift { get { return 1.7f; } }
+    public HoneyManufacture() : base("Honey Manufacturer") { }
+    protected override void DoJob()
+    {
+        HoneyVault.ConvertNectarToHoney(NECTAR_PROCESSED_PER_SHIFT);
+    }
+
+}
